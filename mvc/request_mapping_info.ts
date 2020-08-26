3d6e5770f304d5f4f88a904cacb56cecdc14372e
zcs <530627552@qq.com>
2020-08-26 18:37:31 +0800

    fix

--- mvc/request_mapping_info.ts
+++ mvc/request_mapping_info.ts
@@ -1,4 +1,17 @@
+enum HTTP_METHOD {
+  'GET',
+  'POST',
+  'HEAD',
+  'PUT',
+  'DELETE',
+  'CONNECT',
+  'OPTIONS',
+  'TRACE',
+  'PATCH',
+}
+type METHOD = keyof typeof HTTP_METHOD;
 export default class RequestMappingInfo {
+  private method: METHOD;
   constructor({
     path,
     method = 'GET',
@@ -6,8 +19,23 @@ export default class RequestMappingInfo {
     produces = '',
   }: {
     path: string;
-    method: keyof typeof Methods;
+    method: METHOD;
     consumes: string;
     produces: string;
-  }) {}
+  }) {
+    this.method = method;
+    this.path = new PathResolver(path);
+    this.consume = new ConsumeParser();
+    this.produce = new ProduceParser();
+  }
+
+  match(request: HttpCLi) {
+    return this.PathResolver;
+  }
+}
+interface InfoParser {
+  match(input: req): boolean;
+}
+class PathResolver {
+  resolve() {}
 }
